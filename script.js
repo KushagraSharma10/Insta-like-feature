eb1ca176a7f556f0b26db084ca0e5a89f63d1e4e
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-04-09 22:24:29 +0530

    functionality is completed and program is ready

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const imgContainer = document.querySelector('.insta-img');
+const heart = document.querySelector('i');
 
 imgContainer.addEventListener('dblclick', ()=>{
+   var heartAni = setInterval(() => {
+    heart.style.display = 'block';
     
+
+   }, 50)
+
+   console.log("Heart is popped")
+    setTimeout(() => {
+        clearInterval(heartAni);
+        heart.style.display = 'none';
+
+    },500)
 })
